70ff6f1d15781718282d8ece52483b9d3bd2bd6b
seekingtau <camerontaufink@gmail.com>
2022-03-04 12:46:28 -0600

    Update junoSwap.py

--- junoSwap.py
+++ junoSwap.py
@@ -13,6 +13,12 @@ headers = {
 swapAddressList = []
 decimalList = []
 symbolList = []
+ratioList = []
+tickerRatioList = []
+priceList = []
+tickerPriceList = []
+
+junoPrice = 0
 
 # Probably necessary, I don't know what it does
 def genId():
@@ -65,6 +71,39 @@ def getAssetRatio():
         a = requests.post('https://rpc-juno.itastakers.com/',headers=headers,json=jsonData,timeout=10)
         
         if a.status_code == 200:
-            print(symbolList[i]+" "+str(float(base64.b64decode(json.loads(a.text)['result']['response']['value']).split('"token1_amount":"'.encode('utf-8'),1)[1].split('"}'.encode('utf-8'),1)[0])/1000000))
+            ratioToJuno = str(float(base64.b64decode(json.loads(a.text)['result']['response']['value']).split('"token1_amount":"'.encode('utf-8'),1)[1].split('"}'.encode('utf-8'),1)[0])/1000000)
+            tickerRatioToJuno = symbolList[i] + " " + ratioToJuno
+            
+            print(ratioToJuno)
+            
+            tickerRatioList.append(tickerRatioToJuno)
+            ratioList.append(ratioToJuno)
+            
+def getJunoPrice():
+    global headers
+    global junoPrice
+    
+    jsonData = {"jsonrpc":"2.0","id":genId(),"method":"abci_query","params":{"path":"/cosmwasm.wasm.v1.Query/SmartContractState","data":"0a3f6a756e6f3168756533646e72746766396c793266726e6e7666387a3575376532323463746334686b37776b733278756d65753361726a3672733976677a656312377b22746f6b656e315f666f725f746f6b656e325f7072696365223a7b22746f6b656e315f616d6f756e74223a2231303030303030227d7d","prove":False}}
+    a = requests.post('https://rpc-juno.itastakers.com/',headers=headers,json=jsonData,timeout=10)
+    if a.status_code == 200:
+        junoPrice = round(float(base64.b64decode(json.loads(a.text)['result']['response']['value']).split('"token2_amount":"'.encode('utf-8'),1)[1].split('"}'.encode('utf-8'),1)[0])/1000000,2)
+    
+        ratioList.insert(0, junoPrice)
+        priceList.insert(0, junoPrice)
 
 getAssetRatio()
+getJunoPrice()
+
+# Ratio -> prices
+ratioListLength = len(ratioList)
+
+for i in range(1,ratioListLength):
+    ratioListVal = float(ratioList[i])
+    realPrice = ratioListVal * junoPrice
+    priceList.append(realPrice)
+    
+print(priceList)
+
+for i in range(ratioListLength):
+    tickerPrice = str(symbolList[i]) + ' ' + str(priceList[i])
+    print(tickerPrice)
