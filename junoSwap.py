8629d7cca41fe84e92c6dd7dba6052121aa21ae3
seekingtau <camerontaufink@gmail.com>
2022-03-04 12:20:19 -0600

    Update junoSwap.py

--- junoSwap.py
+++ junoSwap.py
@@ -3,9 +3,6 @@ import random
 import json
 import base64
 import string
-import pandas as pd
-import ssl
-import urllib
 
 # Headers and lists
 headers = {
@@ -26,11 +23,10 @@ def genId():
     return int(res)
 
 # JSON reader
-urlContext = ssl._create_unverified_context()
 url = 'https://raw.githubusercontent.com/CosmosContracts/junoswap-asset-list/main/token_list.json'
-jsonData = urllib.request.urlopen(url, context = urlContext)
+jsonData = requests.get(url,headers={'Host':'raw.githubusercontent.com','Content-Type':'application/json'})
 
-jsonProcessed = json.load(jsonData)
+jsonProcessed = json.loads(jsonData.text)
 
 tokenData = jsonProcessed['tokens']
 
@@ -44,31 +40,31 @@ for x in range(0, tokenListLength):
     swapAddressList.append(swapAddress)
     decimalList.append(decimalCount)
     symbolList.append(tokenSymbol)
-    
-print(swapAddressList)
-print(decimalList)
-print(symbolList)
 
 # Ratios
 def getAssetRatio():
     global headers
     global swapAddressList
     
-    for x in swapAddressList:
-        rawCommand = '\n?' + x + '7{"token2_for_token1_price":{"token2_amount": "1000000"}}'
+    for i in range(len(swapAddressList)):
+        if swapAddressList[i] == "":
+            continue
+        tokenAmount = '1'
+
+        for z in range(decimalList[i]):
+            tokenAmount = tokenAmount +'0'
+            
+        rawCommand = '\n?' + swapAddressList[i] + ''+str(decimalList[i]+1)+'{"token2_for_token1_price":{"token2_amount":"'+tokenAmount+'"}}'
         encodedTerm = rawCommand.encode('UTF-8')
         b16EncodedTerm = base64.b16encode(encodedTerm)
         data = str(b16EncodedTerm, encoding = 'UTF-8')
         newData = data.lower()
-        print(newData)
-    
-    jsonData = {"jsonrpc": "2.0", "id": genId(), "method": "abci_query", "params": {"path": "/cosmwasm.wasm.v1.Query/SmartContractState", "data": newData, "prove": False}}
-    
-    rq = requests.post('https://rpc-juno.itastakers.com/', headers = headers, json = jsonData, timeout = 10)
-    
-    print(rq.text)
-
-    if rq.status_code == 200:
-        print(round(float(base64.b64decode(json.loads(rq.text)['result']['response']['value']).split('"token2_amount":"'.encode('utf-8'),1)[1].split('"}'.encode('utf-8'),1)[0])/1000000,2))
+        
+        jsonData = {"jsonrpc":"2.0","id":genId(),"method":"abci_query","params":{"path":"/cosmwasm.wasm.v1.Query/SmartContractState","data":newData,"prove":False}}
+        
+        a = requests.post('https://rpc-juno.itastakers.com/',headers=headers,json=jsonData,timeout=10)
+        
+        if a.status_code == 200:
+            print(symbolList[i]+" "+str(float(base64.b64decode(json.loads(a.text)['result']['response']['value']).split('"token1_amount":"'.encode('utf-8'),1)[1].split('"}'.encode('utf-8'),1)[0])/1000000))
 
 getAssetRatio()
